75291742eea52366865ffe1eed17dccefd2663c7
pcshen <michelleshen@hahow.in>
2025-02-11 16:12:04 +0800

    feat: add page path to ga event

--- src/app/components/web-vitals/web-vitals.tsx
+++ src/app/components/web-vitals/web-vitals.tsx
@@ -3,18 +3,24 @@
 import { useReportWebVitals } from 'next/web-vitals';
 
 const WebVitals = () => {
+  const currentPath = window.location.pathname;
+  console.log('currentPath:', currentPath);
+
   useReportWebVitals((metric) => {
     console.table(metric);
     // Use `window.gtag` if you initialized Google Analytics as this example:
     // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics
 
-    window.gtag('event', metric.name, {
-      value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value), // values must be integers
-      event_label: metric.id, // id unique to current page load
-      non_interaction: true, // avoids affecting bounce rate.
-    });
+    if (typeof window.gtag === 'function') {
+      window.gtag('event', metric.name, {
+        value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value), // values must be integers
+        event_label: metric.id, // id unique to current page load
+        non_interaction: true, // avoids affecting bounce rate.
+        path: currentPath,
+      });
 
-    console.log('ga event sent: ', metric.name);
+      console.log('ga event sent: ', metric.name);
+    }
   });
 
   return null;
