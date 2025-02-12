f05ac9b18f7447e9468de79c193e0555aa3817b3
pcshen <michelleshen@hahow.in>
2025-02-12 13:17:02 +0800

    fix: prevent accessing window location pathname on server side

--- src/app/components/web-vitals/web-vitals.tsx
+++ src/app/components/web-vitals/web-vitals.tsx
@@ -3,15 +3,14 @@
 import { useReportWebVitals } from 'next/web-vitals';
 
 const WebVitals = () => {
-  const currentPath = window.location.pathname;
-  console.log('currentPath:', currentPath);
-
   useReportWebVitals((metric) => {
     console.table(metric);
     // Use `window.gtag` if you initialized Google Analytics as this example:
     // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics
 
     if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+      const currentPath = window.location.pathname;
+
       window.gtag('event', metric.name, {
         value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value), // values must be integers
         event_label: metric.id, // id unique to current page load
